b2164ad533bc9f1d2b3024ea9a9e9884838285ab
fran-itria <francoitria01@gmail.com>
2026-09-04 01:50:28 -0300

    Add new SVG icons for various technologies
    - Added Mercadopago.svg
    - Added Nest.Js.svg
    - Added PostgreSql.svg
    - Added React.svg
    - Added Sequelize.svg
    - Added TailwindCSS.svg
    - Added TypeOrm.svg
    - Added Typescript.svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-        icon()
+        icon({
+            iconDir: "src/svg"
+        })
     ]
 });
